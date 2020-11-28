fe3c080a0e61f53a758e4c2a0de54833fc1136b7
Moses Karunia <moseskarunia@gmail.com>
2020-11-28 19:57:28 +0700

    mobile, tab (left & right)

---
+++ lib/src/layout_config.dart
@@ -0,0 +1,109 @@
+import 'dart:math';
+
+import 'package:flutter/widgets.dart';
+
+/// Build informations screen sizes.
+/// This object is recalculated on each build call.
+class LayoutConfig {
+  final int screenWidth;
+
+  /// Width of the left column.
+  final int leftColumnWidth;
+
+  /// Width of the drawer.
+  final double drawerWidth;
+
+  /// Width of the right column.
+  final int rightColumnWidth;
+
+  final int centerColumnWidth;
+
+  /// Width of the endDrawer.
+  final double endDrawerWidth;
+
+  /// When false, the left and right column won't be visible,
+  /// no matter their respective widths.
+  final bool isLeftColumnVisible, isRightColumnVisible;
+
+  /// Edge padding to make the center scrollable from the edge of the screen.
+  /// The same value will be substracted from both left and right column width
+  /// (if any). Default value = 16.
+  final int edgePadding;
+
+  int get centerLeftHorizontalPadding => edgePadding + leftColumnWidth;
+  int get centerRightHorizontalPadding => edgePadding + rightColumnWidth;
+
+  const LayoutConfig({
+    @required this.centerColumnWidth,
+    @required this.drawerWidth,
+    this.edgePadding = 16,
+    @required this.endDrawerWidth,
+    @required this.isLeftColumnVisible,
+    @required this.isRightColumnVisible,
+    @required this.leftColumnWidth,
+    @required this.rightColumnWidth,
+    @required this.screenWidth,
+  });
+
+  /// Build layout config from [screenWidth]. The center will be calculated
+  /// with ceiling rounding first.
+  ///
+  /// * [drawerToWidthRatio] ratio of drawer to the total width of the screen.
+  /// * [edgePadding] The space on the left and right of the screen to make
+  ///   the center scrollable from the edge. The same amount will automatically
+  ///   substracted from left and right width (if not already 0)
+  /// * [leftFlex] will be ignored in mobile size.
+  /// * [rightFlex] will be ignored in mobile size. Will be ignored in tablet
+  ///   when [leftFlex] is not 0.
+
+  factory LayoutConfig.build(
+    int screenWidth, {
+    int centerFlex = 2,
+    int centerHorizontalPadding = 16,
+    double drawerToScreenWidthRatio = 0.75,
+    int edgePadding = 16,
+    double endDrawerToScreenWidthRatio = 0.75,
+    int leftFlex = 1,
+    int rightFlex = 1,
+  }) {
+    if (screenWidth < 600) {
+      // Mobile
+      return LayoutConfig(
+        drawerWidth: drawerToScreenWidthRatio * screenWidth,
+        endDrawerWidth: drawerToScreenWidthRatio * screenWidth,
+        isLeftColumnVisible: false,
+        isRightColumnVisible: false,
+        leftColumnWidth: 0,
+        rightColumnWidth: 0,
+        centerColumnWidth: screenWidth,
+        screenWidth: screenWidth,
+        edgePadding: edgePadding,
+      );
+    } else if (screenWidth < 768) {
+      // Tablet
+      int totalFlex =
+          leftFlex > 0 ? centerFlex + leftFlex : centerFlex + rightFlex;
+
+      final leftWidth = max((leftFlex * screenWidth / totalFlex).floor(), 0);
+      final rightWidth = leftFlex == 0
+          ? max((rightFlex * screenWidth / totalFlex).floor(), 0)
+          : 0;
+      final centerWidth = screenWidth - (leftWidth + rightWidth);
+
+      return LayoutConfig(
+        drawerWidth: drawerToScreenWidthRatio * screenWidth,
+        endDrawerWidth: drawerToScreenWidthRatio * screenWidth,
+        isLeftColumnVisible: leftFlex > 0,
+        isRightColumnVisible: leftFlex == 0 && rightFlex > 0,
+        leftColumnWidth: max(leftWidth - edgePadding, 0),
+        rightColumnWidth: max(rightWidth - edgePadding, 0),
+        centerColumnWidth: centerWidth,
+        screenWidth: screenWidth,
+        edgePadding: edgePadding,
+      );
+    } else {
+      // Desktop
+      throw UnimplementedError();
+    }
+  }
+}
---
+++ test/src/layout_config_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:simply_responsive/src/layout_config.dart';
+
+void main() {
+  test('should return mobile configuration', () {
+    final config = LayoutConfig.build(500);
+
+    expect(config.centerColumnWidth, 500);
+    expect(config.centerLeftHorizontalPadding, 16);
+    expect(config.centerRightHorizontalPadding, 16);
+    expect(config.drawerWidth, 0.75 * 500);
+    expect(config.edgePadding, 16);
+    expect(config.endDrawerWidth, 0.75 * 500);
+    expect(config.isLeftColumnVisible, false);
+    expect(config.isRightColumnVisible, false);
+    expect(config.leftColumnWidth, 0);
+    expect(config.rightColumnWidth, 0);
+    expect(config.screenWidth, 500);
+  });
+
+  group('should return tablet configuration', () {
+    test('with left column', () {
+      final config = LayoutConfig.build(750);
+
+      final leftWidth = (1 * 750 / 3).floor();
+      final centerWidth = 750 - leftWidth;
+
+      expect(config.centerColumnWidth, centerWidth);
+      expect(config.centerLeftHorizontalPadding, leftWidth);
+      expect(config.centerRightHorizontalPadding, 16);
+      expect(config.drawerWidth, 0.75 * 750);
+      expect(config.edgePadding, 16);
+      expect(config.endDrawerWidth, 0.75 * 750);
+      expect(config.isLeftColumnVisible, true);
+      expect(config.isRightColumnVisible, false);
+      expect(config.leftColumnWidth, leftWidth - 16);
+      expect(config.rightColumnWidth, 0);
+      expect(config.screenWidth, 750);
+    });
+    test('with right column instead of left', () {
+      final config = LayoutConfig.build(750, leftFlex: 0);
+
+      final rightWidth = (1 * 750 / 3).floor();
+      final centerWidth = 750 - rightWidth;
+
+      expect(config.centerColumnWidth, centerWidth);
+      expect(config.centerLeftHorizontalPadding, 16);
+      expect(config.centerRightHorizontalPadding, rightWidth);
+      expect(config.drawerWidth, 0.75 * 750);
+      expect(config.edgePadding, 16);
+      expect(config.endDrawerWidth, 0.75 * 750);
+      expect(config.isLeftColumnVisible, false);
+      expect(config.isRightColumnVisible, true);
+      expect(config.leftColumnWidth, 0);
+      expect(config.rightColumnWidth, rightWidth - 16);
+      expect(config.screenWidth, 750);
+    });
+    test('with center column only', () {
+      //
+    });
+  });
+}
